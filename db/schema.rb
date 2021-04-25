a715d1bc9b9b5e1e2f1c8147f71740acde305a72
Kristina Markina <kr.markina@gmail.com>
2021-04-24 21:30:14 -0400

    migrated body for posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210424221855) do
+ActiveRecord::Schema.define(version: 20210425012814) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "body"
   end
 
 end
